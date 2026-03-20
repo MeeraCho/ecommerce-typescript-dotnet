97b1ad685c9e7438cb1906a1dd93137141cea3a2
MiraCho <miracho119@gmail.com>
2026-03-20 13:26:31 -0600

    feat: add links to the NavBar

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca29807a198c8c5ec8cc1f14bc7daf981a73509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a594d171ea218db4f71e33d04a321a2634339a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
