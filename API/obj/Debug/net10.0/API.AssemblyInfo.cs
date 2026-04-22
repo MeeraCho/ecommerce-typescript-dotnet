3c08390fd68764ace54083bad428823c7f93cdd8
MiraCho <miracho119@gmail.com>
2026-04-22 14:30:37 -0600

    feat: show correct item count on shopping cart badge

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bcfe0f0493b27dbf7ae5a8cf05c37aa762d415a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc347de6dd5d20e4acf69e9f83de3d643539e08f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
