30b64e199f26ffcfa93bfbebeeb89ea2bdcc14cf
MiraCho <miracho119@gmail.com>
2026-04-25 20:05:31 -0600

    feat: add filtering to the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,11 +8,12 @@ namespace API.Controllers
     public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string? orderBy, string? searchTerm)
+        public async Task<ActionResult<List<Product>>> GetProducts(string? orderBy, string? searchTerm, string? brands, string? types)
         {
             var query = context.Products
                         .Sort(orderBy)
                         .Search(searchTerm)
+                        .Filter(brands, types)
                         .AsQueryable();
             return await query.ToListAsync();
         }
