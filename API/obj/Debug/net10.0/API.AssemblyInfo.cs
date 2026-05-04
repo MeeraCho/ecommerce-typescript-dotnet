9231c716d4f8da8ef22b340e9ace3daee9af844e
MiraCho <miracho119@gmail.com>
2026-05-04 10:42:01 -0600

    feat: use the type and brand filters on the client

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba60698a111b5ba9fc25ef0dbf1e10be24e0fbcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14717607c4dd149345b2a01dd8e12c3bb96cfdb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
