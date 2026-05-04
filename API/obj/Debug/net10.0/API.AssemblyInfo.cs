ba60698a111b5ba9fc25ef0dbf1e10be24e0fbcd
MiraCho <miracho119@gmail.com>
2026-05-03 21:50:57 -0600

    feat: add the search functionality to the client

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d72a7ae513cb41197bd4b194d499edc4114cb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717cca240859064466bc44f9b0ce418270586bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
