14717607c4dd149345b2a01dd8e12c3bb96cfdb6
MiraCho <miracho119@gmail.com>
2026-05-03 23:03:00 -0600

    feat: add the ordering functionality to the client

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717cca240859064466bc44f9b0ce418270586bd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba60698a111b5ba9fc25ef0dbf1e10be24e0fbcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
