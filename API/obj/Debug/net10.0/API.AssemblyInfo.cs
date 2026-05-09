ad316bfa21ce9e5e75d30289169650cd5bbb5803
MiraCho <miracho119@gmail.com>
2026-05-09 11:03:55 -0600

    clean up unnecessary files

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354663d5867250228e1f0d2edbe057a0a6f5783c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e40b19171c66a7b4941c822accf34bb5ab95649c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
