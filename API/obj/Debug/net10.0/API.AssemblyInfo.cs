345407a921b078083e770807fc9cabd4b9f25751
MiraCho <miracho119@gmail.com>
2026-05-15 23:16:55 -0600

    feat: install Stripe

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09913d6f4623dc65e02d69ad21561da496ca5a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17d716e39b22e376919a3d19cf332ba20c2208b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
