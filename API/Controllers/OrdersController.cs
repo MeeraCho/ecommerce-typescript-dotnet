3ecb21bfcb1306a7a32434172d473a2d6dc08a80
MiraCho <miracho119@gmail.com>
2026-05-31 23:57:06 -0600

    feat: add CreateOrder Http POST request endpoint

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -34,4 +34,104 @@ public class OrderController(StoreContext context) : BaseApiController
         return order;
     }
 
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+    {
+        // 1. get uers's basket
+        var basket = await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+        // validate uers's basket 
+        if ( basket == null ||                                // Basket doesn't exists
+                basket.Items.Count == 0 ||                    //Basket doesn't contains items
+                string.IsNullOrEmpty(basket.PaymentIntentId)) //Stripe PaymentIntent doesn't exists
+            return BadRequest("Basket is empty or not found"); 
+                
+        // 2. convert basket items to order items 
+        var items = CreateOrderItems(basket.Items);
+
+        // validate uers's basket item     
+        if (items == null) return BadRequest("Some items out of stock");
+
+        // 3. calculate totals
+        var subtotal = items.Sum(x => x.Price * x.Quantity);
+        var deliveryFee = CalculateDeliveryFee(subtotal);
+            
+            // 4. check for existing order (idempotency)
+        var order = await context.Orders
+            .Include(x => x.OrderItems) //retrieves all associated OrderItems
+            .FirstOrDefaultAsync(x => x.PaymentIntentId == basket.PaymentIntentId);
+    
+            // 5. If order does NOT exist, create new order OR update order if exists
+        if (order == null)
+        {
+            order = new Order
+            {
+                BuyerEmail = User.GetUsername(),           //the email address of the logged-in user.
+                ShippingAddress = orderDto.ShippingAddress,//the delivery address provided by the customer.
+                OrderItems = items,                        //the products being purchased.
+                Subtotal = subtotal,
+                DeliveryFee = deliveryFee,            
+                PaymentIntentId = basket.PaymentIntentId,   //the Stripe PaymentIntent identifier used to link the order to the payment.
+                PaymentSummary = orderDto.PaymentSummary,  //payment details from the checkout form.        
+            };
+                    
+                    // Adds the order to EF Core tracking.
+            context.Orders.Add(order);
+            
+            // cleans up
+            context.Baskets.Remove(basket); //The customer's basket is removed from the database because it is no longer needed after the order has been created.
+            Response.Cookies.Delete("basketId"); //The basket cookie is deleted from the user's browser since the basket no longer exists.
+        }
+        else 
+        {
+            // If order already exists → update items
+            order.OrderItems = items;
+        }   
+    
+            // 6. Save changes to database
+        var result = await context.SaveChangesAsync() > 0;
+            
+            // 7. Handle failure - if SaveChangesAsync() returns 0, no changes were saved to the database.
+        if (!result) return BadRequest("Something went wrong while creating the order.r");
+            
+            // 8. Return success response	
+        return CreatedAtAction(
+                    nameof(GetOrderDetails), //specifies the action that can retrieve the resource
+                    new { id = order.Id },   //supplies route values
+                    order.ToDto());          //return newly created order data
     }
+
+        private long CalculateDeliveryFee(long subtotal)
+    {
+        return subtotal > 10000 ? 0 : 500;
+    }
+
+
+    private List<OrderItem>? CreateOrderItems(List<BasketItem> items)
+    {
+        var orderItems = new List<OrderItem>();
+
+        foreach (var item in items)
+        {
+            if (item.Product.QuantityInStock < item.Quantity)
+                return null;
+
+            var orderItem = new OrderItem
+            {
+                ItemOrdered = new ProductItemOrdered
+                {
+                    ProductId = item.ProductId,
+                    PictureUrl = item.Product.PictureUrl,
+                    Name = item.Product.Name
+                },
+                Price = item.Product.Price,
+                Quantity = item.Quantity
+            };
+            orderItems.Add(orderItem);
+
+            item.Product.QuantityInStock -= item.Quantity;
+        }
+
+        return orderItems;
+    }
+}
--- API/DTOs/OrderDto.cs
+++ API/DTOs/OrderDto.cs
@@ -7,13 +7,13 @@ public class OrderDto
 {
     public int Id { get; set; }
     public required string BuyerEmail { get; set; }
-    public DateTime OrderDate { get; set; }     
     public required ShippingAddress ShippingAddress { get; set; }
-    public required PaymentSummary PaymentSummary { get; set; }
-    public long DeliveryFee { get; set; }
-    public long Subtotal { get; set; }
-    public required string OrderStatus { get; set; }
-    public long Total { get; set; }
-    public long Discount { get; set; }
+    public DateTime OrderDate { get; set; }     
     public List<OrderItemDto> OrderItems { get; set; } = [];
+    public long Subtotal { get; set; }
+    public long DeliveryFee { get; set; }
+    public long Discount { get; set; }
+    public required string OrderStatus { get; set; }
+    public required PaymentSummary PaymentSummary { get; set; }
+    public long Total { get; set; }
 }
