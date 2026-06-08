7c25bcc898f0837918b725cf1ae041b0c04f8f3d
MiraCho <miracho119@gmail.com>
2026-06-07 23:36:32 -0600

    add HandlePaymentIntentSucceeded, HandlePaymentIntentFailed method for Stripe Webhook

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,9 +1,12 @@
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers;
 
@@ -29,5 +32,57 @@ public class PaymentsController(PaymentsService paymentsService, StoreContext co
         }
 
         return basket.ToDto();
+    }
+
+    [HttpPost("webhook")]
+    public async Task<IActionResult> StripeWebhook()
+    {
+        
+    }
+
+    private async Task HandlePaymentIntentFailed(PaymentIntent intent)
+    {
+        var order = await context.Orders
+            .Include(x => x.OrderItems)
+            .FirstOrDefaultAsync(x => x.PaymentIntentId == intent.Id)
+                ?? throw new Exception("Order not found");
+
+        foreach (var item in order.OrderItems)
+        {
+            var productItem = await context.Products
+                .FindAsync(item.ItemOrdered.ProductId)
+                    ?? throw new Exception("Problem updating order stock");
+
+            productItem.QuantityInStock += item.Quantity;
+        }
+
+        order.OrderStatus = OrderStatus.PaymentFailed;
+
+        await context.SaveChangesAsync();
+    }
+
+
+    private async Task HandlePaymentIntentSucceeded(PaymentIntent intent)
+    {
+        var order = await context.Orders
+            .Include(x => x.OrderItems)
+            .FirstOrDefaultAsync(x => x.PaymentIntentId == intent.Id)
+                ?? throw new Exception("Order not found");
+
+        if (order.GetTotal() != intent.Amount)
+        {
+            order.OrderStatus = OrderStatus.PaymentMismatch;
+        }
+        else
+        {
+            order.OrderStatus = OrderStatus.PaymentReceived;
+        }
+
+        var basket = await context.Baskets.FirstOrDefaultAsync(x => 
+            x.PaymentIntentId == intent.Id);
+            
+        if (basket != null) context.Baskets.Remove(basket);
+
+        await context.SaveChangesAsync();
     }
 }
