dab8a9d02f603863086c547cc1669836f9d5ee45
MiraCho <miracho119@gmail.com>
2026-06-26 22:32:27 -0600

    add Product CRUD functionality

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -77,5 +77,22 @@ namespace API.Controllers
 
             return BadRequest("Problem updating product");
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            context.Products.Remove(product);
+
+            var result = await context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem deleting the product");
+        }
     }
 }
