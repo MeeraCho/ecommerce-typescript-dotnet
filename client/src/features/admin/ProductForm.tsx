49086e6ac3bd52e4c79fdc6933fd4185f92f3983
MiraCho <miracho119@gmail.com>
2026-07-06 00:16:41 -0600

    Create a Reusable Text Input

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,15 +1,15 @@
 //import { zodResolver } from "@hookform/resolvers/zod"
-import {  type CreateProductSchema } from "../../lib/Schemas/createProductSchema"
-import { Controller, useForm } from "react-hook-form"
-import { Paper, Typography, Grid, TextField, Box, Button } from "@mui/material"
+//import {  type CreateProductSchema } from "../../lib/Schemas/createProductSchema"
+import { useForm, type FieldValues } from "react-hook-form"
+import { Paper, Typography, Grid, Box, Button } from "@mui/material"
+import AppTextInput from "../../app/shared/components/AppTextInput"
 
 export default function ProductForm() {
-  const { control, handleSubmit } = useForm<CreateProductSchema>({
-    mode: 'onTouched', //언제 에러를 검사할 거냐? input을 한 번이라도 클릭(blur)하면 그 이후부터 validation 실행
+  const { control, handleSubmit } = useForm({
+    mode: 'onTouched', 
     // resolver: zodResolver(createProductSchema) //검증 엔진을 Zod로. React Hook Form 기본 validation은 너무 간단
   })
-  const onSubmit = (data: CreateProductSchema) => console.log(data)
-
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   return (
     <Box component={Paper} sx={{p: 4, maxWidth: 'lg', mx: 'auto'}}>
@@ -21,12 +21,7 @@ export default function ProductForm() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={3}>
           <Grid size={12}>
-              <Controller
-                render={({ field }) => <TextField {...field} fullWidth label="name"/>}
-                name="name"
-                control={control}
-                defaultValue=""
-              />
+              <AppTextInput control={control} name="name" label="Product name" />
           </Grid>
         </Grid>
 
