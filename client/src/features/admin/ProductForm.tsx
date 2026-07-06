233b2b097e9ad1754f82b9c107766817e90208c7
MiraCho <miracho119@gmail.com>
2026-07-06 00:28:12 -0600

    Create the Product Form Content

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,15 +1,15 @@
-//import { zodResolver } from "@hookform/resolvers/zod"
-//import {  type CreateProductSchema } from "../../lib/Schemas/createProductSchema"
-import { useForm, type FieldValues } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { createProductSchema, type CreateProductSchema } from "../../lib/Schemas/createProductSchema"
+import { useForm } from "react-hook-form"
 import { Paper, Typography, Grid, Box, Button } from "@mui/material"
 import AppTextInput from "../../app/shared/components/AppTextInput"
 
 export default function ProductForm() {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<CreateProductSchema>({
     mode: 'onTouched', 
-    // resolver: zodResolver(createProductSchema) //검증 엔진을 Zod로. React Hook Form 기본 validation은 너무 간단
+    resolver: zodResolver(createProductSchema) //검증 엔진을 Zod로. React Hook Form 기본 validation은 너무 간단
   })
-  const onSubmit = (data: FieldValues) => console.log(data)
+  const onSubmit = (data: CreateProductSchema) => console.log(data)
 
   return (
     <Box component={Paper} sx={{p: 4, maxWidth: 'lg', mx: 'auto'}}>
@@ -23,6 +23,24 @@ export default function ProductForm() {
           <Grid size={12}>
               <AppTextInput control={control} name="name" label="Product name" />
           </Grid>
+          <Grid size={6}>
+            <AppTextInput control={control} name="brand" label="Brand" />
+          </Grid>
+          <Grid size={6}>
+            <AppTextInput control={control} name="type" label="Type" />
+          </Grid>
+          <Grid size={6}>
+            <AppTextInput control={control} name="price" label="Price" type="number" />
+          </Grid>
+          <Grid size={6}>
+            <AppTextInput control={control} name="quantityInStock" label="Quantity in Stock" type="number"/>
+          </Grid>
+          <Grid size={12}>
+            <AppTextInput control={control} name="description" label="Description" multiline rows={4}/>
+          </Grid>
+          <Grid size={12}>
+            <AppTextInput control={control} name="file" label="Image" />
+          </Grid>            
         </Grid>
 
         <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
